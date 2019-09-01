51db734ceb8a3a8824d270d229e0a92f8cf5fc01
Luis Fabian <luisfabian@Luiss-MacBook-Air.local>
2019-09-01 18:39:33 -0400

    ML

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,8 @@
+"""
+Will explore the idea of programs that optimize through a process that mimics Natural selection. 'The Fittest Survives'
+We need:
+1. Problem to solve ?
+2. A fitness function. What makes a good solution?
+3. A way to represent solutions similar to how DNA represents living organisms.
+"""
+
