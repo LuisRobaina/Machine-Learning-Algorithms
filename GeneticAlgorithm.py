136a6e955a8b0eb8b04ad1e3e13d57347c046ae0
Luis Fabian <luisfabian@Luiss-MacBook-Air.local>
2019-09-02 10:36:22 -0400

    ML

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -6,3 +6,33 @@ We need:
 3. A way to represent solutions similar to how DNA represents living organisms.
 """
 
+# Read the data from a cvs file
+import pandas as pd
+import random
+
+data = pd.read_csv('data.csv')
+
+# Our programs will have a set of symbols they can use
+# A valid program will have a pattern such as [m,x,a,+,m]
+# which translates to F = (m*a)+m (Which is incorrect).
+
+def _first_gen(size: int) -> [any]:
+    """ Randomly generate the first generation of operations"""
+    pass
+
+def _fit(individual):
+    pass
+
+def _isoperation(symbol):
+    pass
+def _valid_operation(operation):
+    pass
+def evolve(data):
+    """
+    As we evolve our operations will keep track of the fittest individual in the population
+    Will evolve by randomly matting individuals in the populations, as well as randomnly mutating excisting individuals.
+    """
+    pass
+    # Find the two fittest individuals, cross them and add the new individuals to the population.
+
+
